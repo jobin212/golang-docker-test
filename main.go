f7689aaa3162e0b6e249c31a8d787321477e6395
jobin <jobin212@gmail.com>
2019-08-21 22:58:44 -0700

    writing to database

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 
@@ -36,6 +38,7 @@ func main() {
 
 	router.HandleFunc("/ping", PingDB).Methods("GET")
 	router.HandleFunc("/events", GetEvents).Methods("GET")
+	router.HandleFunc("/createEvent", CreateEvent).Methods("GET")
 	router.HandleFunc("/", PingDB).Methods("GET")
 
 	fmt.Println("Listening on port 8080-- hello world!")
@@ -60,4 +63,20 @@ func GetEvents(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "events ping succeeded")
+}
+
+func CreateEvent(w http.ResponseWriter, r *http.Request) {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	t1 := r1.Intn(100)
+
+	sqlStatement := `
+	INSERT INTO jobin212.users (age, email, first_name, last_name)
+	VALUES ($1, $2, $3, $4)`
+	_, err := db.Exec(sqlStatement, t1, "xxyy@email.com", "Joe", "Smith")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "Jos Smith of age %d inserted into database", t1)
 }
