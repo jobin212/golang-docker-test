29f93d402be7a58addeea12e28200fef6081520a
jobin <jobin212@gmail.com>
2019-08-24 11:50:20 -0700

    can get single events by id now

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 
@@ -37,11 +38,46 @@ func main() {
 
 	router.HandleFunc("/ping", PingDB).Methods("GET")
 	router.HandleFunc("/events", GetEvents).Methods("GET")
+	router.HandleFunc("/events/{id:[0-9]+}", GetEvent).Methods("GET")
 	router.HandleFunc("/events", CreateEvent).Methods("POST")
 	router.HandleFunc("/", PingDB).Methods("GET")
 
-	fmt.Println("Listening on port 8080-- hello world!")
+	fmt.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func GetEvent(w http.ResponseWriter, r *http.Request) {
+	// get mux vars
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+
+	// first get by email, then write if empty
+	sqlStatement := `SELECT age, first_name, last_name, email FROM jobin212.users WHERE id=$1;`
+
+	row := db.QueryRow(sqlStatement, id)
+
+	user_obj := user_model{ID: id}
+
+	if err = row.Scan(&user_obj.Age, &user_obj.FirstName, &user_obj.LastName, &user_obj.Email); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			http.Error(w, "There is no user associated with this id", http.StatusNotFound)
+			return
+		default:
+			http.Error(w, "There is no user associated with this id", http.StatusInternalServerError)
+		}
+	}
+
+	// fmt.Fprintf(w, "User exists %s, %s", user_obj.FirstName, user_obj.LastName)
+	respondWithJSON(w, 200, user_obj)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
 
 func PingDB(w http.ResponseWriter, r *http.Request) {
@@ -71,11 +107,19 @@ type user_creation_request struct {
 	LastName  string
 }
 
+type user_model struct {
+	Age       int    `json:"age"`
+	Email     string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	ID        int    `json:"id"`
+}
+
 func CreateEvent(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 
-	var user user_creation_request
-	err := decoder.Decode(&user)
+	var request user_creation_request
+	err := decoder.Decode(&request)
 	if err != nil {
 		panic(err)
 	}
@@ -83,7 +127,7 @@ func CreateEvent(w http.ResponseWriter, r *http.Request) {
 	// first get by email, then write if empty
 	sqlStatement := `SELECT id FROM jobin212.users WHERE email=$1;`
 	var id int
-	row := db.QueryRow(sqlStatement, user.Email)
+	row := db.QueryRow(sqlStatement, request.Email)
 
 	switch err = row.Scan(&id); err {
 	case sql.ErrNoRows:
@@ -100,10 +144,11 @@ func CreateEvent(w http.ResponseWriter, r *http.Request) {
 	sqlStatement = `
 	INSERT INTO jobin212.users (age, email, first_name, last_name)
 	VALUES ($1, $2, $3, $4)`
-	_, err = db.Exec(sqlStatement, user.Age, user.Email, user.FirstName, user.LastName)
+	_, err = db.Exec(sqlStatement, request.Age, request.Email, request.FirstName, request.LastName)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Fprintf(w, "First Name: %s, Last Name: %s, Email: %s, Age: %d", user.FirstName, user.LastName, user.Email, user.Age)
+	fmt.Fprintf(w, "First Name: %s, Last Name: %s, Email: %s, Age: %d",
+		request.FirstName, request.LastName, request.Email, request.Age)
 }
