94915e14c8eb1643a0d410ef6a3752d3a921a289
jobin <jobin212@gmail.com>
2019-08-24 11:17:37 -0700

    remove misc events code

--- main.go
+++ main.go
@@ -5,10 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
-	"strconv"
-	"time"
 
 	"github.com/gorilla/mux"
 
@@ -40,7 +37,7 @@ func main() {
 
 	router.HandleFunc("/ping", PingDB).Methods("GET")
 	router.HandleFunc("/events", GetEvents).Methods("GET")
-	router.HandleFunc("/events", CreateEventFromBody).Methods("POST")
+	router.HandleFunc("/events", CreateEvent).Methods("POST")
 	router.HandleFunc("/", PingDB).Methods("GET")
 
 	fmt.Println("Listening on port 8080-- hello world!")
@@ -67,22 +64,6 @@ func GetEvents(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "events ping succeeded")
 }
 
-func CreateEvent(w http.ResponseWriter, r *http.Request) {
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
-	t1 := r1.Intn(100)
-
-	sqlStatement := `
-	INSERT INTO jobin212.users (age, email, first_name, last_name)
-	VALUES ($1, $2, $3, $4)`
-	_, err := db.Exec(sqlStatement, t1, "xxyy"+strconv.Itoa(t1)+"@email.com", "Joe", "Smith")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Fprintf(w, "Jos Smith of age %d inserted into database", t1)
-}
-
 type user_creation_request struct {
 	Age       int
 	Email     string
@@ -90,7 +71,7 @@ type user_creation_request struct {
 	LastName  string
 }
 
-func CreateEventFromBody(w http.ResponseWriter, r *http.Request) {
+func CreateEvent(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 
 	var user user_creation_request
